590d0839a9f64e3bc0078de8fd8a77e1df2e4c87
John Miguel Dizon <jdizon11@my.bcit.ca>
2023-04-06 14:44:22 -0700

    added new note and cancel feature

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,24 @@ const aboutSelector = document.querySelector("#about")
 
 const docsSelector = document.querySelector("#docs")
 
+const inputSelector = document.querySelector("input")
+
+const noteList = document.querySelector("#noteList")
+
+const saveButton = document.querySelector(".save")
+
+const cancelButton = document.querySelector(".cancel")
+
+const boxSelector = document.querySelector(".box textarea")
+
+const newButton = document.querySelector(".new")
 //arrays
 
 let notesArray = []
 
 
 //functions
-function darkMode (event)
+function darkMode ()
 {
 
     bodySelector.classList.toggle("maindarkBG")
@@ -26,6 +37,8 @@ function darkMode (event)
     mainSelector.classList.toggle("whiteText")
     docsSelector.classList.toggle("whiteText")
     aboutSelector.classList.toggle("whiteText")
+    boxSelector.classList.toggle("sidedarkBG")
+
 
 
     if(darkModeButton.textContent == "Dark Theme")
@@ -40,21 +53,49 @@ function darkMode (event)
 
 }
 
-function addNote()
+function addToNote()
 {
 
     const titleInput = document.querySelector("")
-    const noteInput = document.querySelector("")
+    const noteInput = document.querySelector("inputSelector")
     let note = {
         title: titleInput.value,
         body: noteInput.value
     }
     notesArray.push(note)
+    titleInput.value = ""
+    noteInput.value = ""
+    const newLi = document.createElement("li")
+    newLi.textContent = `${note.title}: ${note.body}`
+    noteList.appendChild(newLi)
+
 }
 
+function cancelNote(event)
+{
 
+}
 
-
-
+function noteCreateDelete(event) {
+    if (event.target.textContent == "Cancel")
+    {
+        boxSelector.classList.add("hideElement")
+        saveButton.classList.add("hideElement")
+        cancelButton.classList.add("hideElement")
+    }
+    else if (event.target.textContent == "New note")
+    {
+        boxSelector.classList.remove("hideElement")
+        saveButton.classList.remove("hideElement")
+        cancelButton.classList.remove("hideElement")
+    }
+}
 //event listeners
 darkModeButton.addEventListener("click",darkMode)
+
+saveButton.addEventListener("click",addToNote)
+
+cancelButton.addEventListener("click", noteCreateDelete)
+
+newButton.addEventListener("click", noteCreateDelete)
+
